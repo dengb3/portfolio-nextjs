4adaa3f5d2490a73b6fa02dced9b967ca4f2bb83
Deng Benjamin <Deng.benjamin.6@gmail.com>
2023-07-03 15:52:39 -0600

    -updated pages

---
+++ app/About/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <div>Welcome About!</div>;
+}
+
+export default About;
---
+++ app/Projects/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Projects() {
+  return <div>Welcome Projects!</div>;
+}
+
+export default Projects;
