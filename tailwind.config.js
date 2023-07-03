6ce3b617c7a7a3f25811e45f176999b52ba9dd31
Deng Benjamin <Deng.benjamin.6@gmail.com>
2023-07-03 17:51:27 -0600

    - added conditioinal coloring and animation for h3

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,52 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        'pop-in': {
+          '0%': {
+            opacity: '0',
+            transform: 'scale(0)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'scale(1)',
+          },
+        },
+        'rapid-fire': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-50%)',
+          },
+          '20%': {
+            opacity: '1',
+            transform: 'translateX(0%)',
+          },
+          '80%': {
+            opacity: '1',
+            transform: 'translateX(0%)',
+          },
+          '100%': {
+            opacity: '0',
+            transform: 'translateX(50%)',
+          },
+        },
+        'fade-in': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(10px)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        'pop-in': 'pop-in 0.5s ease-in-out forwards',
+        'rapid-fire': 'rapid-fire 2s ease-in-out infinite',
+        'fade-in': 'fade-in 0.5s ease-out',
+      },
     },
   },
   plugins: [],
-}
+};
