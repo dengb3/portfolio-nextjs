bd6ce3bf5b1f4163c56a7adb35c194faf090919f
Deng Benjamin <Deng.benjamin.6@gmail.com>
2023-07-04 11:42:56 -0600

    test

--- app/pages/contact.jsx
+++ app/pages/contact.jsx
@@ -5,6 +5,7 @@ function Contact() {
   return (
     <section className="container mx-auto py-10 px-4">
       <Heading title="Let's get in touch" />
+      <di>Hello this is a test!</di>
     </section>
   );
 }
