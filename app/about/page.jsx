92ef35c2f216a068448c4d0b0d7a4638570a0197
Deng Benjamin <Deng.benjamin.6@gmail.com>
2023-07-09 18:09:31 -0600

    - changed img to Image from next

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
 
 function About() {
   const images = [
@@ -74,10 +75,11 @@ function About() {
                 className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30"
               >
                 <div className="h-96 w-72">
-                  <img
+                  <Image
                     className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
                     alt=""
                     src={image.src}
+                    layout="fill"
                   />
                 </div>
                 <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
@@ -110,10 +112,11 @@ function About() {
             className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30"
           >
             <div className="h-96 w-72">
-              <img
+              <Image
                 className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
                 alt=""
                 src={image.src}
+                layout="fill"
               />
             </div>
             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
